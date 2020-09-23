7ff7af4089da1776b2a5adb8254b687d14033251
Ujjawal <ujjawal.raptor@gmail.com>
2020-09-24 05:12:32 +0530

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBNEhUDfORu7k4AzmPZ1W4bNzraCIP5bCU",
+    authDomain: "whatsapp-mern-2e9f7.firebaseapp.com",
+    databaseURL: "https://whatsapp-mern-2e9f7.firebaseio.com",
+    projectId: "whatsapp-mern-2e9f7",
+    storageBucket: "whatsapp-mern-2e9f7.appspot.com",
+    messagingSenderId: "1028514100425",
+    appId: "1:1028514100425:web:cdc0efae67009a82ae8918",
+    measurementId: "G-YQ3HB7PW0G"
+  };
